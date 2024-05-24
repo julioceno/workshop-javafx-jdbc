6c4ba899e46cbc3678c8202bba48980d5d2be0ce
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-23 22:03:55 -0300

    main view

--- src/main/java/org/example/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/org/example/workshopjavafxjdbc/HelloApplication.java
@@ -2,6 +2,7 @@ package org.example.workshopjavafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+        Parent parent = loader.load();
+        Scene mainScene = new Scene(parent);
+        stage.setTitle("Sample JavaFX application");
         stage.show();
     }
 
---
+++ src/main/java/org/example/workshopjavafxjdbc/gui/MainView.java
@@ -0,0 +1,4 @@
+package org.example.workshopjavafxjdbc.gui;
+
+public class MainView {
+}
