801f30f192ef04d4a45363cb91dd4bb69930e163
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-25 14:33:56 -0300

    [feature] Seller form

--- src/main/java/org/example/workshopjavafxjdbc/SellerFormController.java
+++ src/main/java/org/example/workshopjavafxjdbc/SellerFormController.java
@@ -3,10 +3,7 @@ package org.example.workshopjavafxjdbc;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.example.workshopjavafxjdbc.db.DbException;
 import org.example.workshopjavafxjdbc.listerners.DataChangeListener;
 import org.example.workshopjavafxjdbc.model.entities.Department;
@@ -19,6 +16,8 @@ import org.example.workshopjavafxjdbc.utils.Constraints;
 import org.example.workshopjavafxjdbc.utils.Utils;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
 
 public class SellerFormController implements Initializable {
@@ -36,13 +35,31 @@ public class SellerFormController implements Initializable {
     private TextField txtName;
 
     @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private DatePicker dpBirthDate;
+
+    @FXML
+    private TextField txtBaseSalary;
+
+    @FXML
     private Label labelErrorName;
 
     @FXML
-    private Button btSave;
+    private Label labelErrorEmail;
 
     @FXML
-    private Button btCancel;
+    private Label labelErrorBirthDate;
+
+    @FXML
+    private Label labelErrorBaseSalary;
+
+    @FXML
+    private Button buttonSave;
+
+    @FXML
+    private Button buttonCancel;
 
     public void setSeller(Seller entity) {
         this.entity = entity;
@@ -57,7 +74,7 @@ public class SellerFormController implements Initializable {
     }
 
     @FXML
-    public void onBtSaveAction(ActionEvent event) {
+    public void onButtonSaveAction(ActionEvent event) {
         if (entity == null) {
             throw new IllegalStateException("Entity was null");
         }
@@ -104,7 +121,7 @@ public class SellerFormController implements Initializable {
     }
 
     @FXML
-    public void onBtCancelAction(ActionEvent event) {
+    public void onButtonCancelAction(ActionEvent event) {
         Utils.currentStage(event).close();
     }
 
@@ -115,7 +132,10 @@ public class SellerFormController implements Initializable {
 
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
-        Constraints.setTextFieldMaxLength(txtName, 30);
+        Constraints.setTextFieldMaxLength(txtName, 70);
+        Constraints.setTextFieldDouble(txtBaseSalary);
+        Constraints.setTextFieldMaxLength(txtEmail, 60);
+        Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
     }
 
     public void updateFormData() {
@@ -124,6 +144,12 @@ public class SellerFormController implements Initializable {
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+        txtEmail.setText(entity.getEmail());
+        Locale.setDefault(Locale.US);
+        txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+        if (entity.getBirthDate() != null) {
+            dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+        }
     }
 
     private void setErrorMessages(Map<String, String> errors) {
